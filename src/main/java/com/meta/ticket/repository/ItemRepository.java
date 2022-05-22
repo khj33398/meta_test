e7b5f160dfe10371f63ba95b760133ef3587f197
khj33398 <khj33398@naver.com>
2022-05-23 00:13:21 +0900

    jpa test

--- src/main/java/com/meta/ticket/repository/ItemRepository.java
+++ src/main/java/com/meta/ticket/repository/ItemRepository.java
@@ -2,12 +2,21 @@ package com.meta.ticket.repository;
 
 import com.meta.ticket.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
-    List<Item> findByName(String name);
+public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item> {
+    //List<Item> findByName(String name);
     List<Item> findById(long id);
 
+    //Entity 객체를 대상으로 쿼리를 수행하므로 데이터베이스에 독립적
+    @Query("select i from Item i where i.name like %:testname% order by i.id desc")
+    List<Item> findByName(@Param("testname") String testname);
 
+    //nativeQuery : 특정 데이터베이스에 종속적
+    @Query(value = "select * from item i where i.name like %:testname% order by i.id desc ", nativeQuery = true)
+    List<Item> findByNameByNative(@Param("testname") String testname);
 }
--- src/test/java/com/meta/ticket/repository/ItemRepositoryTest.java
+++ src/test/java/com/meta/ticket/repository/ItemRepositoryTest.java
@@ -1,12 +1,22 @@
 package com.meta.ticket.repository;
 
 import com.meta.ticket.domain.Item;
+import com.meta.ticket.domain.QItem;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.TestPropertySource;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -32,22 +42,105 @@ class ItemRepositoryTest {
     }
 
     public void createItemList(){
-        for(int i=1; i<=10; i++){
+        for(int i=1; i<=89; i++){
             Item item = new Item();
             item.setName("테스트 상품"+i);
-            item.setPrice(10000+i);
+            item.setPrice(10010+i);
             Item savedItem = itemRepository.save(item);
         }
     }
 
-    @Test
+    //@Test
     @DisplayName("상품명 조회 테스트")
     public void findByIdTest(){
-        //this.createItemList();
+        this.createItemList();
         //List<Item> itemList = itemRepository.findByName("테스트 상품3");
-        List<Item> itemList = itemRepository.findById(4);
+        List<Item> itemList = itemRepository.findById(75);
         for(Item tmp:itemList){
             System.out.println(tmp.toString());
         }
+    }
+
+    //@Test
+    @DisplayName("모든 상품 조회")
+    public void findAllTest(){
+        List<Item> list = itemRepository.findAll();
+        for(Item item:list){
+            System.out.println(item.toString());
+        }
+    }
+
+    //@Test
+    @DisplayName("@Query를 이용한 상품 조회 테스트")
+    public void findByNameTest(){
+        List<Item> list = itemRepository.findByName("상품6");
+        for(Item item : list){
+            System.out.println(item.toString());
+        }
+    }
+
+    //@Test
+    @DisplayName("@NativeQuery를 이용한 상품 조회 테스트")
+    public void findByNameByNativeTest(){
+        List<Item> list = itemRepository.findByNameByNative("상품7");
+        for(Item item : list){
+            System.out.println(item.toString());
+        }
+    }
+
+    @PersistenceContext
+    EntityManager em;
+
+    //@Test
+    //repository 인터페이스에 미리 선언해놓지 않고 실행 시점에 쿼리할 수 있음
+    @DisplayName("Querydsl 조회 테스트")
+    public void queryDslTest(){
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em); // repository 유사
+        QItem qItem = QItem.item; // entity 객체 유사
+        JPAQuery<Item> query = queryFactory.selectFrom(qItem).
+                where(qItem.name.eq("테스트 상품18")).
+                orderBy(qItem.id.desc());
+
+        List<Item> list = query.fetch(); //fetch() 메서드 실행 시점에 쿼리문이 실행됨
+
+        for(Item item : list){
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("Querydsl 조회 테스트 2")
+    public void queryDslTest2(){
+        /*
+        Item item = new Item();
+        item.setName("테스트 상품150");
+        item.setPrice(10028);
+        itemRepository.save(item);
+        */
+
+        //쿼리에 들어갈 조건을 만들어주는 빌더
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        QItem qItem = QItem.item;
+
+        int price = 10035;
+        String name = "테스트 상품18";
+
+        //booleanBuilder.and(qItem.name.eq(name));
+        booleanBuilder.and(qItem.price.gt(price));
+
+        Sort sort1 = Sort.by("id").descending();
+        Sort sort2 = Sort.by("name").descending();
+        Sort sort3 = sort1.and(sort2);
+
+        //Pageable pageable = PageRequest.of(1,5);
+        Pageable pageable = PageRequest.of(1,5, sort1);
+        Page<Item> page = itemRepository.findAll(booleanBuilder, pageable);
+        System.out.println(page.getTotalElements());
+
+        List<Item> list = page.getContent();
+        for(Item temp:list){
+            System.out.println(temp.toString());
+        }
+
     }
 }
